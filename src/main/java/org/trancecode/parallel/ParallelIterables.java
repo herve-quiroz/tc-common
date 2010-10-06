b3c373e74b133fabbcd2f73c007874de21b4c12a
Herve Quiroz <herve.quiroz@trancecode.org>
2010-10-06 22:50:39 +0200

    Use ParallelIterators.transform()

--- src/main/java/org/trancecode/parallel/ParallelIterables.java
+++ src/main/java/org/trancecode/parallel/ParallelIterables.java
@@ -20,49 +20,32 @@ package org.trancecode.parallel;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
-import com.google.common.util.concurrent.ValueFuture;
 
-import java.util.Queue;
-import java.util.concurrent.Callable;
+import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-
-import org.trancecode.collection.TcIterables;
-import org.trancecode.function.TcPredicates;
 
 /**
+ * Parallel equivalent of {@link Iterables}.
+ * 
  * @author Herve Quiroz
  */
 public final class ParallelIterables
 {
-    private static Future<?> LAST = ValueFuture.create();
-
-    private static <T> Future<T> last()
-    {
-        @SuppressWarnings("unchecked")
-        final Future<T> last = (Future<T>) LAST;
-        return last;
-    }
-
     /**
      * @see Iterables#transform(Iterable, Function)
      */
     public static <F, T> Iterable<T> transform(final Iterable<F> fromIterable,
             final Function<? super F, ? extends T> function, final ExecutorService executor)
     {
-        // TODO the logic should probably be moved to some Iterator instead
-
-        final Function<F, Callable<T>> applyFunction = ParallelFunctions.apply(function);
-        final Iterable<Callable<T>> callables = Iterables.transform(fromIterable, applyFunction);
-
-        final Queue<Future<T>> futures = new LinkedBlockingQueue<Future<T>>();
-        final Function<Callable<T>, Future<T>> submitFunction = ParallelFunctions.submit(executor);
-        Iterables.addAll(futures, Iterables.transform(callables, submitFunction));
-        final Future<T> last = last();
-        futures.add(last);
-
-        return getUntilLast(futures);
+        return new Iterable<T>()
+        {
+            @Override
+            public Iterator<T> iterator()
+            {
+                return ParallelIterators.transform(fromIterable.iterator(), function, executor);
+            }
+        };
     }
 
     /**
@@ -73,14 +56,6 @@ public final class ParallelIterables
     {
         // TODO
         return Iterables.filter(unfiltered, predicate);
-    }
-
-    private static <T> Iterable<T> getUntilLast(final Iterable<Future<T>> futures)
-    {
-        final Future<T> last = last();
-        final Iterable<Future<T>> futuresUntilLast = TcIterables.until(futures, TcPredicates.identicalTo(last));
-        final Function<Future<T>, T> function = ParallelFunctions.get();
-        return Iterables.transform(futuresUntilLast, function);
     }
 
     public static <T> Iterable<T> get(final Iterable<Future<T>> futures)
