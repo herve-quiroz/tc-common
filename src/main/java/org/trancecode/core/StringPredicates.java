0766fddd1f1402f3c594757b344b3775b70108a6
Herve Quiroz <herve.quiroz@trancecode.org>
2010-10-13 23:39:48 +0200

    Add StringPredicates.endsWith()

---
+++ src/main/java/org/trancecode/core/StringPredicates.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2010 TranceCode Software
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+package org.trancecode.core;
+
+import com.google.common.base.Predicate;
+
+/**
+ * {@link Predicate} implementations related to {@link String}.
+ * 
+ * @author Herve Quiroz
+ */
+public final class StringPredicates
+{
+    /**
+     * @see String#endsWith(String)
+     */
+    public static Predicate<String> endsWith(final String suffix)
+    {
+        return new Predicate<String>()
+        {
+            @Override
+            public boolean apply(final String string)
+            {
+                return string.endsWith(suffix);
+            }
+        };
+    }
+
+    private StringPredicates()
+    {
+        // No instantiation
+    }
+}
