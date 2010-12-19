d8efb80a537de573fd3328a81d6ff6563799d127
Herve Quiroz <herve.quiroz@trancecode.org>
2010-12-20 00:32:09 +0100

    Add TcAssert.assertEquasl() for XdmNode instances

--- src/test/java/org/trancecode/TcAssert.java
+++ src/test/java/org/trancecode/TcAssert.java
@@ -18,7 +18,15 @@
 package org.trancecode;
 
 import com.google.common.collect.ImmutableList;
+import junit.framework.AssertionFailedError;
+import net.sf.saxon.dom.NodeOverNodeInfo;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XdmNode;
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.testng.Assert;
+import org.trancecode.xml.saxon.Saxon;
+import org.w3c.dom.Document;
 
 /**
  * {@link Assert} extensions.
@@ -30,5 +38,29 @@ public final class TcAssert
     public static <T> void assertIterableEquals(final Iterable<T> actual, final Iterable<T> expected)
     {
         Assert.assertEquals(ImmutableList.copyOf(actual), ImmutableList.copyOf(expected));
+    }
+
+    public static void assertEquals(final XdmNode expected, final XdmNode actual)
+    {
+        assertEquals(expected, actual, true);
+    }
+
+    public static void assertEquals(final XdmNode expected, final XdmNode actual, final boolean ignoreWhitespace)
+    {
+        assert expected != null;
+        assert actual != null;
+        final Processor processor = (Processor) expected.getUnderlyingNode().getConfiguration().getProcessor();
+        final XdmNode docExpected = Saxon.asDocumentNode(expected, processor);
+        final XdmNode docActual = Saxon.asDocumentNode(actual, processor);
+        XMLUnit.setIgnoreWhitespace(ignoreWhitespace);
+        try
+        {
+            XMLAssert.assertXMLEqual((Document) NodeOverNodeInfo.wrap(docExpected.getUnderlyingNode()),
+                    (Document) NodeOverNodeInfo.wrap(docActual.getUnderlyingNode()));
+        }
+        catch (final AssertionFailedError e)
+        {
+            Assert.fail(String.format("expected:\n%s\nactual:\n%s", docExpected, docActual), e);
+        }
     }
 }
