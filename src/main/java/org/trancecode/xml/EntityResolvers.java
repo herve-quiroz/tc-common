561c2aba9e8f824ea7c5ea7322af501eb84c94f8
Herve Quiroz <herve.quiroz@trancecode.org>
2011-01-10 22:17:29 +0100

    Use my own name in file headers

--- src/main/java/org/trancecode/xml/EntityResolvers.java
+++ src/main/java/org/trancecode/xml/EntityResolvers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 TranceCode Software
+ * Copyright (C) 2008 Herve Quiroz
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
