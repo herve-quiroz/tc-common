f0514b14b55e0ee58e74b6d4c196d48182d93bf9
Herve Quiroz <herve.quiroz@trancecode.org>
2011-03-10 22:40:29 +0100

    Add TcLists.immutableList()

---
+++ src/main/java/org/trancecode/collection/TcLists.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2011 Herve Quiroz
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+package org.trancecode.collection;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+
+import java.util.List;
+
+/**
+ * Utility methods related to {@link List}.
+ * 
+ * @author Herve Quiroz
+ */
+public final class TcLists
+{
+    public static <T> List<T> immutableList(final Iterable<T> elements, final T newElement)
+    {
+        return ImmutableList.copyOf(Iterables.concat(elements, ImmutableList.of(newElement)));
+    }
+
+    private TcLists()
+    {
+        // No instantiation
+    }
+}
