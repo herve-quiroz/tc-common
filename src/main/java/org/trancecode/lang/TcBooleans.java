afab0ab5a4ce50530174a85dd4e0a06f39c3e68c
Herve Quiroz <herve.quiroz@trancecode.org>
2011-03-12 17:17:23 +0100

    Add TcBooleans.getValue()

---
+++ src/main/java/org/trancecode/lang/TcBooleans.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2011 Herve Quiroz
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+package org.trancecode.lang;
+
+import org.trancecode.api.Nullable;
+
+/**
+ * Utility methods related to {@code Boolean}.
+ * 
+ * @author Herve Quiroz
+ */
+public final class TcBooleans
+{
+    public static boolean getValue(@Nullable final Boolean value)
+    {
+        return value != null && value;
+    }
+
+    private TcBooleans()
+    {
+        // No instantiation
+    }
+}
