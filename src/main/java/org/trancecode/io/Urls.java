743de0922b563e002ffd3b21ae155458ef624d0f
Herve Quiroz <herve.quiroz@trancecode.org>
2011-05-24 19:45:38 +0200

    Add Urls.asInputSupplier()

---
+++ src/main/java/org/trancecode/io/Urls.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2011 Herve Quiroz
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+package org.trancecode.io;
+
+import com.google.common.base.Preconditions;
+import com.google.common.io.InputSupplier;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Utility methods related to {@link URL}.
+ * 
+ * @author Herve Quiroz
+ */
+public final class Urls
+{
+    public static InputSupplier<InputStream> asInputSupplier(final URL url)
+    {
+        Preconditions.checkNotNull(url);
+
+        return new InputSupplier<InputStream>()
+        {
+            @Override
+            public InputStream getInput() throws IOException
+            {
+                return url.openStream();
+            }
+        };
+    }
+
+    private Urls()
+    {
+        // No instantiation
+    }
+}
